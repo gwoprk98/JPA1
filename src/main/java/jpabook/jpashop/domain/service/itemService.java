3add5ab22762808a3fbd576614311dd04263c8cd
gwoprk98 <gwoprk98@gmail.com>
2025-04-01 22:07:57 +0900

    feat: 상품

---
+++ src/main/java/jpabook/jpashop/domain/service/itemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.repository.itemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class itemService {
+
+    private final itemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
