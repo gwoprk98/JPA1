2ef8207fa4118c24962d3d309d114f36ed5e8c91
gwoprk98 <gwoprk98@gmail.com>
2025-04-09 22:32:47 +0900

    feat: 페이징

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.BatchSize;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
